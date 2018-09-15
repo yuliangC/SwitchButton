3207708b3f0098655caeb467721c6be56ba2909f
Silence <1510720220@qq.com>
2018-09-15 16:52:52 +0800

    文字和触摸反馈事件已完成

--- app/src/main/java/com/example/silence/switchbutton/MainActivity.java
+++ app/src/main/java/com/example/silence/switchbutton/MainActivity.java
@@ -2,14 +2,26 @@ package com.example.silence.switchbutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.library.SwitchButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SwitchButton switchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        switchButton=findViewById(R.id.switchButton);
+        switchButton.setListener(new SwitchButton.OnSwitchCheckListener() {
+            @Override
+            public void checkButton(int tabIndex, String selectedString) {
+                String string="选中了第"+tabIndex+"个tab，选中文字为："+selectedString;
+                Toast.makeText(MainActivity.this,string,Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
 
--- library/src/main/java/com/example/library/SwitchButton.java
+++ library/src/main/java/com/example/library/SwitchButton.java
@@ -12,8 +12,11 @@ import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import static com.example.library.UIUtils.dip2Px;
@@ -25,11 +28,14 @@ import static com.example.library.UIUtils.sp2px;
 public class SwitchButton extends View {
 
 
-    private int normalColor,selectedColor,selectedTab;
+    private int normalColor;
+    private int selectedColor;
+    private int selectedTab;
     private float textSize,strokeRadius,strokeWidth,totalWidth,totalHeight;
     private String[] tabTexts=new String[]{"左边","中间","右边","右边"};
     private Paint selectedPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
-    private Paint normalPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Paint textPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
+    private OnSwitchCheckListener listener;
 
 
     public SwitchButton(Context context) {
@@ -44,6 +50,7 @@ public class SwitchButton extends View {
     public SwitchButton(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         TypedArray array=context.obtainStyledAttributes(attrs,R.styleable.SwitchButton);
+        normalColor=array.getColor(R.styleable.SwitchButton_normalColor, Color.WHITE);
         selectedColor=array.getColor(R.styleable.SwitchButton_selectedColor, Color.RED);
         selectedTab=array.getInt(R.styleable.SwitchButton_selectedTab,0);
         textSize=array.getDimension(R.styleable.SwitchButton_textSize, sp2px(context,14));
@@ -51,6 +58,69 @@ public class SwitchButton extends View {
         strokeWidth=array.getDimension(R.styleable.SwitchButton_strokeWidth, dip2Px(context,2));
         array.recycle();
     }
+
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int defaultWidth = getDefaultWidth();
+        int defaultHeight = getDefaultHeight();
+        setMeasuredDimension(getExpectSize(defaultWidth, widthMeasureSpec), getExpectSize(defaultHeight,
+                heightMeasureSpec));
+    }
+
+    /**
+     * get default height when android:layout_height="wrap_content"
+     */
+    private int getDefaultHeight() {
+        Paint.FontMetrics fontMetrics=textPaint.getFontMetrics();
+        return (int) (fontMetrics.bottom - fontMetrics.top) + getPaddingTop() + getPaddingBottom();
+    }
+
+    /**
+     * get default width when android:layout_width="wrap_content"
+     */
+    private int getDefaultWidth() {
+        float tabTextWidth = 0f;
+        int tabs = tabTexts.length;
+        for (int i = 0; i < tabs; i++) {
+            tabTextWidth = Math.max(tabTextWidth, textPaint.measureText(tabTexts[i]));
+        }
+        float totalTextWidth = tabTextWidth * tabs;
+        float totalStrokeWidth = (strokeWidth * tabs);
+        int totalPadding = (getPaddingRight() + getPaddingLeft()) * tabs;
+        return (int) (totalTextWidth + totalStrokeWidth + totalPadding);
+    }
+
+
+    /**
+     * get expect size
+     *
+     * @param size
+     * @param measureSpec
+     * @return
+     */
+    private int getExpectSize(int size, int measureSpec) {
+        int result = size;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+        switch (specMode) {
+            case MeasureSpec.EXACTLY:
+                result = specSize;
+                break;
+            case MeasureSpec.UNSPECIFIED:
+                result = size;
+                break;
+            case MeasureSpec.AT_MOST:
+                result = Math.min(size, specSize);
+                break;
+            default:
+                break;
+        }
+        return result;
+    }
+
+
 
 
     /**
@@ -64,7 +134,7 @@ public class SwitchButton extends View {
         //逐个画矩形兵处理两边圆角。画笔宽度有一半会融入图形大小，另外一半在图形之外
         selectedPaint.setColor(selectedColor);
         selectedPaint.setStrokeWidth(strokeWidth);
-        selectedPaint.setAntiAlias(true);
+        textPaint.setTextSize(textSize);
         float tabWidth=totalWidth/tabTexts.length;
         for (int i=0;i<tabTexts.length;i++){
             if (selectedTab==i){
@@ -79,58 +149,31 @@ public class SwitchButton extends View {
             }else {
                 canvas.drawPath(getCenterTabPath(tabWidth,i),selectedPaint);
             }
+            drawText(canvas,i,tabWidth);
         }
 
-
-        //画文字 还有触摸反馈事件
-
-
-
-
-
-
-
-
-//        //画外框和分割线
-//        RectF rect=new RectF(0,0,getMeasuredWidth(),getMeasuredHeight());
-//        selectedPaint.setStrokeWidth(strokeWidth);
-//        selectedPaint.setColor(selectedColor);
-//        canvas.drawRoundRect(rect,strokeRadius,strokeRadius,selectedPaint);
-//        if (tabTexts.length<=1){
-//            return;
-//        }
-//        float tabWidth=(getMeasuredWidth()-((tabTexts.length+1)*strokeWidth))/tabTexts.length;
-//        //获取背景颜色
-//
-//        for (int i=0;i<tabTexts.length-1;i++){
-//            float width=(tabWidth+strokeWidth)*(i+1);
-//            canvas.drawLine(width,strokeWidth,width,getMeasuredHeight()-strokeWidth,selectedPaint);
-//        }
-//
-//
-//
-//
-//
-//
-//        //画选中部分的背景色
-//        if (selectedTab==0){
-//            RectF rect1=new RectF(0,0,tabWidth+strokeWidth,getMeasuredHeight()-strokeWidth);
-//            canvas.drawRoundRect(rect1,strokeRadius,strokeRadius,selectedPaint);
-//            RectF rectR=new RectF(0,0,strokeRadius,strokeRadius);
-//            canvas.drawRect(rectR,selectedPaint);
-//            selectedPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-//        }else if (selectedTab==tabTexts.length-1){
-//            float startWith=getMeasuredWidth()-tabWidth-strokeWidth;
-//            RectF rect1=new RectF(startWith,strokeWidth,getMeasuredWidth()-strokeWidth,getMeasuredHeight()-strokeWidth);
-//            canvas.drawRoundRect(rect1,strokeRadius,strokeRadius,selectedPaint);
-//            RectF rectR=new RectF(getMeasuredWidth()-strokeRadius-strokeWidth,0,strokeRadius,strokeRadius);
-//            canvas.drawRect(rectR,selectedPaint);
-//            selectedPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-//        }else {
-//
-//        }
-
     }
+
+
+    /**
+     * @param canvas    画布
+     * @param position  tab位置
+     * @param tabWidth  每一个tab宽度
+     */
+    private void drawText(Canvas canvas,int position,float tabWidth){
+        String text=tabTexts[position];
+        if (position==selectedTab){
+            textPaint.setColor(normalColor);
+        }else {
+            textPaint.setColor(selectedColor);
+        }
+        Rect textRect=new Rect();
+        textPaint.getTextBounds(text,0,text.length(),textRect);
+        float startX=position*tabWidth+tabWidth/2-textRect.width()/2;
+        float startY=getMeasuredHeight()/2+textRect.height()/2;
+        canvas.drawText(text,startX,startY,textPaint);
+    }
+
 
 
     /**
@@ -196,12 +239,141 @@ public class SwitchButton extends View {
     }
 
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction()==MotionEvent.ACTION_UP) {
+            float x = event.getX();
+            float y = event.getY();
+            float tabWith = getMeasuredWidth() / tabTexts.length;
+            for (int i = 0; i < tabTexts.length; i++) {
+                float startX = tabWith * i;
+                float endX = tabWith * (i + 1);
+                float startY = strokeWidth / 2;
+                float endY = getMeasuredHeight();
+                if (x > startX && x < endX && y > startY && y < endY) {
+                    selectedTab = i;
+                    invalidate();
+                    if (listener != null) {
+                        listener.checkButton(i, tabTexts[i]);
+                    }
+                    break;
+                }
+            }
+        }
+        return true;
+    }
 
 
 
+    //清楚选中状态
+    public void clearSelectin(){
+        selectedTab=-1;
+        postInvalidate();
+    }
 
 
+    public int getNormalColor() {
+        return normalColor;
+    }
 
+    public void setNormalColor(int normalColor) {
+        this.normalColor = normalColor;
+    }
+
+    public int getSelectedColor() {
+        return selectedColor;
+    }
+
+    public void setSelectedColor(int selectedColor) {
+        this.selectedColor = selectedColor;
+    }
+
+    public int getSelectedTab() {
+        return selectedTab;
+    }
+
+    public void setSelectedTab(int selectedTab) {
+        this.selectedTab = selectedTab;
+    }
+
+    public float getTextSize() {
+        return textSize;
+    }
+
+    public void setTextSize(float textSize) {
+        this.textSize = textSize;
+    }
+
+    public float getStrokeRadius() {
+        return strokeRadius;
+    }
+
+    public void setStrokeRadius(float strokeRadius) {
+        this.strokeRadius = strokeRadius;
+    }
+
+    public float getStrokeWidth() {
+        return strokeWidth;
+    }
+
+    public void setStrokeWidth(float strokeWidth) {
+        this.strokeWidth = strokeWidth;
+    }
+
+    public String[] getTabTexts() {
+        return tabTexts;
+    }
+
+    public void setTabTexts(String... tabTexts) {
+        if (tabTexts.length<=1){
+            throw new IllegalArgumentException("the size of tabTexts should greater then 1");
+        }
+        this.tabTexts = tabTexts;
+    }
+
+    public void setListener(OnSwitchCheckListener listener) {
+        this.listener = listener;
+    }
+
+
+    @Nullable
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        super.onSaveInstanceState();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(SwitchButton.class.getSimpleName(),super.onSaveInstanceState());
+        bundle.putInt("selectedTab", selectedTab);
+        bundle.putStringArray("tabTexts",tabTexts);
+        bundle.putInt("normalColor",normalColor);
+        bundle.putInt("selectedColor",selectedColor);
+        bundle.putFloat("textSize",textSize);
+        bundle.putFloat("strokeRadius",strokeRadius);
+        bundle.putFloat("strokeWidth",strokeWidth);
+        return bundle;
+    }
+
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        if (state instanceof Bundle){
+            Bundle bundle= (Bundle) state;
+            selectedTab=bundle.getInt("selectedTab");
+            tabTexts=bundle.getStringArray("tabTexts");
+            normalColor=bundle.getInt("normalColor");
+            selectedColor=bundle.getInt("selectedColor");
+            textSize=bundle.getFloat("textSize");
+            strokeRadius=bundle.getFloat("strokeRadius");
+            strokeWidth=bundle.getFloat("strokeWidth");
+            super.onRestoreInstanceState(bundle.getParcelable(SwitchButton.class.getSimpleName()));
+        }else {
+            super.onRestoreInstanceState(state);
+        }
+
+    }
+
+    public interface OnSwitchCheckListener{
+        void checkButton(int tabIndex,String selectedString);
+    }
 
 
 }
